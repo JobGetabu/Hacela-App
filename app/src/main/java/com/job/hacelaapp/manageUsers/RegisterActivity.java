8ee2c462a0f3e45b12d1be925792eb41096dcacf
JobGetabu <getabujob@gmail.com>
2018-03-31 13:47:28 +0300

    refined the dependencies and theme

---
+++ app/src/main/java/com/job/hacelaapp/manageUsers/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.job.hacelaapp.manageUsers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.hacelaapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
