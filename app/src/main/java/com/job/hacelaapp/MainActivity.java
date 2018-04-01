bc6629aa980445c58f7a546f6f625e7d0ab3e766
JobGetabu <getabujob@gmail.com>
2018-04-01 21:19:23 +0300

    designed the register Added the registration fragments

--- app/src/main/java/com/job/hacelaapp/MainActivity.java
+++ app/src/main/java/com/job/hacelaapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.job.hacelaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.job.hacelaapp.manageUsers.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //TODO remove this intent
+        Intent regiIntent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(regiIntent);
+
     }
 }
--- app/src/main/java/com/job/hacelaapp/manageUsers/RegisterActivity.java
+++ app/src/main/java/com/job/hacelaapp/manageUsers/RegisterActivity.java
@@ -1,15 +1,56 @@
 package com.job.hacelaapp.manageUsers;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
 
 import com.job.hacelaapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.register_btn_email)
+    ImageButton btnEmail;
+    @BindView(R.id.register_btn_facebook)
+    ImageButton btnFacebook;
+    @BindView(R.id.register_btn_google)
+    ImageButton btnGoogle;
+    @BindView(R.id.register_btn_twitter)
+    ImageButton btnTwitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ButterKnife.bind(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @OnClick(R.id.register_btn_email)
+    public void btnEmailclick(){
+        btnEmail.setImageDrawable(getDrawable(R.drawable.ic_email_white_24dp));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @OnClick(R.id.register_btn_facebook)
+    public void btnFacebookclick(){
+        btnFacebook.setImageDrawable(getDrawable(R.drawable.ic_fb_icon_white));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @OnClick(R.id.register_btn_twitter)
+    public void btnTwitterclick(){
+        btnTwitter.setImageDrawable(getDrawable(R.drawable.ic_twitter_icon_white));
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @OnClick(R.id.register_btn_google)
+    public void btnGoogleclick(){
+        btnGoogle.setImageDrawable(getDrawable(R.drawable.ic_googleplus_icon_white));
     }
 }
