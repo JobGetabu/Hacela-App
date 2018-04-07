ecb72a4fb08eb5e628e6dcdfae2a22432d45ceb9
JobGetabu <getabujob@gmail.com>
2018-04-07 17:12:39 +0300

    Added adapters and manage users fragments

---
+++ app/src/main/java/com/job/hacelaapp/adapter/RegistrationFragmentsAdapter.java
@@ -0,0 +1,43 @@
+package com.job.hacelaapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.job.hacelaapp.manageUsers.EmailPasswordFragment;
+import com.job.hacelaapp.manageUsers.FacebookSignUpFragment;
+import com.job.hacelaapp.manageUsers.GoogleSignUpFragment;
+import com.job.hacelaapp.manageUsers.TwittersignUpFragment;
+
+/**
+ * Created by Job on Sunday : 4/1/2018.
+ */
+public class RegistrationFragmentsAdapter extends FragmentPagerAdapter {
+
+
+    public RegistrationFragmentsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new EmailPasswordFragment();
+            case 1:
+                return new FacebookSignUpFragment();
+            case 2:
+                return new TwittersignUpFragment();
+            case 3:
+                return new GoogleSignUpFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
