d4897aa0188b9a02d17059b82370d1ec36efda37
JobGetabu <getabujob@gmail.com>
2018-04-09 01:40:59 +0300

    added home activity toolbar

--- app/src/main/java/com/job/hacelaapp/MainActivity.java
+++ app/src/main/java/com/job/hacelaapp/MainActivity.java
@@ -3,20 +3,38 @@ package com.job.hacelaapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.job.hacelaapp.manageUsers.LoginActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mainactivity_toolbar)
+    Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolBar);
 
 
         //TODO remove this intent
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_home_menu,menu);
+         return true;
     }
 }
--- app/src/main/java/com/job/hacelaapp/manageUsers/EmailPasswordLogInFragment.java
+++ app/src/main/java/com/job/hacelaapp/manageUsers/EmailPasswordLogInFragment.java
@@ -1,19 +1,33 @@
 package com.job.hacelaapp.manageUsers;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.job.hacelaapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EmailPasswordLogInFragment extends Fragment {
 
+    @BindView(R.id.login_input_email_number)
+    TextInputLayout emailOrNumber;
+    @BindView(R.id.login_input_password)
+    TextInputLayout mPassword;
+
+
+    private View mRootView;
 
     public EmailPasswordLogInFragment() {
         // Required empty public constructor
@@ -24,7 +38,20 @@ public class EmailPasswordLogInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_email_password_log_in, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_email_password_log_in, container, false);
+        ButterKnife.bind(this,mRootView);
+        
+        return  mRootView;
     }
 
+    @OnClick(R.id.login_tv_signup)
+    public void tvLoginSignUp(){
+        Intent registerIntent = new Intent(getActivity(),RegisterActivity.class);
+        startActivity(registerIntent);
+    }
+    
+    @OnClick(R.id.login_tv_forgotpassword)
+    public void tvLoginForgotPassword(){
+        Toast.makeText(getActivity(), "TODO: Implement forgot password", Toast.LENGTH_SHORT).show();
+    }
 }
