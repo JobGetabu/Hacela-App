1457297123c79759f83052d2b38b3772df17b892
JobGetabu <getabujob@gmail.com>
2018-04-23 00:03:14 +0300

    refactored emailpasswordloginfragment

--- app/src/main/java/com/job/hacelaapp/manageUsers/EmailPasswordLogInFragment.java
+++ app/src/main/java/com/job/hacelaapp/manageUsers/EmailPasswordLogInFragment.java
@@ -3,11 +3,11 @@ package com.job.hacelaapp.manageUsers;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +28,7 @@ import com.job.hacelaapp.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -85,23 +86,6 @@ public class EmailPasswordLogInFragment extends Fragment {
         mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(mainIntent);
     }
-    private void UserAuthToastExceptions(@NonNull Task<AuthResult> authtask) {
-        String error = "";
-        try {
-            throw authtask.getException();
-        } catch (FirebaseAuthWeakPasswordException e) {
-            error = "Weak Password!";
-        } catch (FirebaseAuthInvalidCredentialsException e) {
-            error = "Invalid email";
-            Log.d(TAG, "UserAuthToastExceptions: ", e);
-        } catch (FirebaseAuthUserCollisionException e) {
-            error = "Existing Account";
-        } catch (Exception e) {
-            error = "Unknown Error Occured";
-            e.printStackTrace();
-        }
-        Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
-    }
 
     @OnClick(R.id.login_btn_signup)
     public void loginWithEmailPasswordClick(){
@@ -111,13 +95,21 @@ public class EmailPasswordLogInFragment extends Fragment {
 
         Log.d(TAG, "loginWithEmailPasswordClick: Email & password:"+email+"   "+password);
 
-        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+        if (validate()){
 
+            /*
             mdialog = new ProgressDialog(getActivity());
             mdialog.setTitle("Log in");
             mdialog.setMessage("Please wait logging in...");
             mdialog.setCanceledOnTouchOutside(false);
             mdialog.show();
+            */
+
+            final SweetAlertDialog pDialog = new SweetAlertDialog(getActivity(), SweetAlertDialog.PROGRESS_TYPE);
+            pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+            pDialog.setTitleText("Logging in...");
+            pDialog.setCancelable(false);
+            pDialog.show();
 
             mAuth.signInWithEmailAndPassword(email,password)
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -126,17 +118,74 @@ public class EmailPasswordLogInFragment extends Fragment {
 
                             if (authtask.isSuccessful()){
 
-                                mdialog.dismiss();
+                                //mdialog.dismiss();
+                                pDialog.dismissWithAnimation();
                                 sendToMain();
                             }else {
+                                //mdialog.dismiss();
+                                pDialog.dismiss();
                                 UserAuthToastExceptions(authtask);
-                                mdialog.dismiss();
                             }
 
                         }
                     });
         }else {
-            //TODO: Error prompting logic
+            // Error prompting logic ... Done!
         }
+    }
+
+    private void errorPrompt(){
+        new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
+                .setTitleText("Oops...")
+                .setContentText("Something went wrong!")
+                .show();
+    }
+
+    private void errorPrompt(String title,String message){
+        new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
+                .setTitleText(title)
+                .setContentText(message)
+                .show();
+    }
+
+    private void UserAuthToastExceptions(@NonNull Task<AuthResult> authtask) {
+        String error = "";
+        try {
+            throw authtask.getException();
+        } catch (FirebaseAuthWeakPasswordException e) {
+            error = "Weak Password!";
+        } catch (FirebaseAuthInvalidCredentialsException e) {
+            error = "Invalid email";
+        } catch (FirebaseAuthUserCollisionException e) {
+            error = "Existing Account";
+        } catch (Exception e) {
+            error = "Unknown Error Occured";
+            e.printStackTrace();
+        }
+        //Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
+        errorPrompt("Oops...",error);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = logEmail.getEditText().toString();
+        String password = mPassword.getEditText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            logEmail.setError("enter a valid email address");
+            valid = false;
+        } else {
+            logEmail.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6 ) {
+            mPassword.setError("between at least 6 characters");
+            valid = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        return valid;
     }
 }
