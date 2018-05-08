39948d7608ea9dbf50f6c155eee380495ba25856
JobGetabu <getabujob@gmail.com>
2018-05-08 18:53:30 +0300

    group creation init

---
+++ app/src/main/java/com/job/hacelaapp/viewmodel/CreateGroupViewModel.java
@@ -0,0 +1,9 @@
+package com.job.hacelaapp.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Job on Tuesday : 5/8/2018.
+ */
+public class CreateGroupViewModel extends ViewModel {
+}
