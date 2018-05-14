a79a0a50b2900c0034279c49fb1c5f1e9324f1ca
JobGetabu <getabujob@gmail.com>
2018-05-14 14:42:09 +0300

    group creation wizards UI done

--- app/src/main/java/com/job/hacelaapp/ui/CreateGroupActivity.java
+++ app/src/main/java/com/job/hacelaapp/ui/CreateGroupActivity.java
@@ -67,4 +67,18 @@ public class CreateGroupActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+            //additional code
+        } else {
+            getFragmentManager().popBackStack();
+        }
+
+    }
 }
