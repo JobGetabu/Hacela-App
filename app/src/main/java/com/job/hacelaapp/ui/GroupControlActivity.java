650db2d4d377612b9440f6fe3560247ce554a021
JobGetabu <getabujob@gmail.com>
2018-05-24 23:56:31 +0300

    group control UI animating :)

--- app/src/main/java/com/job/hacelaapp/ui/GroupControlActivity.java
+++ app/src/main/java/com/job/hacelaapp/ui/GroupControlActivity.java
@@ -1,9 +1,20 @@
 package com.job.hacelaapp.ui;
 
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.job.hacelaapp.R;
+
+import java.util.ArrayList;
 
 public class GroupControlActivity extends AppCompatActivity {
 
@@ -11,5 +22,122 @@ public class GroupControlActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_control);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+
+
+        final AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+
+            final View headerImage = findViewById(R.id.header_image);
+            final View headerInfo = findViewById(R.id.header_info);
+            final View avatar = findViewById(R.id.avatar_border);
+            final LinearLayout texts = (LinearLayout) findViewById(R.id.texts);
+
+            final int avatarHOffset = getResources().getDimensionPixelSize(R.dimen.profile_avatar_h_offset);
+            final int avatarVOffset = getResources().getDimensionPixelSize(R.dimen.profile_avatar_v_offset);
+            final int avatarSize = getResources().getDimensionPixelSize(R.dimen.profile_avatar_size);
+            final int textHOffset = getResources().getDimensionPixelSize(R.dimen.profile_texts_h_offset);
+            final int textVMinOffset = getResources().getDimensionPixelSize(R.dimen.profile_texts_v_min_offset);
+            final int textVMaxOffset = getResources().getDimensionPixelSize(R.dimen.profile_texts_v_max_offset);
+            final int textVDiff = textVMaxOffset - textVMinOffset;
+            final int header160 = getResources().getDimensionPixelSize(R.dimen.dp160);
+            final int toolBarHeight;
+
+            {
+                final TypedArray styledAttributes = getTheme().obtainStyledAttributes(
+                        new int[] { android.R.attr.actionBarSize });
+                toolBarHeight = (int) styledAttributes.getDimension(0, 0) + getStatusBarHeight();
+                styledAttributes.recycle();
+
+                avatar.setPivotX(0);
+                avatar.setPivotY(0);
+                texts.setPivotX(0);
+                texts.setPivotY(0);
+            }
+
+            final ArrayList<Float> textStart = new ArrayList<>();
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                final int diff = toolBarHeight + verticalOffset;
+                final int y = diff < 0 ? header160 - diff : header160;
+                headerInfo.setTop(y);
+
+                final FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) headerImage.getLayoutParams();
+                lp.height = y;
+                headerImage.setLayoutParams(lp);
+
+                final int totalScrollRange = appBarLayout.getTotalScrollRange();
+                final float ratio = ((float)totalScrollRange + verticalOffset) / totalScrollRange;
+
+                final int avatarHalf = avatar.getMeasuredHeight() / 2;
+                final int avatarRightest = appBarLayout.getMeasuredWidth() / 2 - avatarHalf - avatarHOffset;
+                final int avatarTopest = avatarHalf + avatarVOffset;
+
+                avatar.setX(avatarHOffset + avatarRightest * ratio);
+                avatar.setY(avatarVOffset - avatarTopest * ratio);
+                avatar.setScaleX(0.5f + 0.5f * ratio);
+                avatar.setScaleY(0.5f + 0.5f * ratio);
+
+                if (textStart.isEmpty() && verticalOffset == 0) {
+                    for (int i = 0; i < texts.getChildCount(); i++) {
+                        textStart.add(texts.getChildAt(i).getX());
+                    }
+                }
+
+                texts.setX(textHOffset + (avatarSize * 0.5f - avatarVOffset) * (1f - ratio));
+                texts.setY(textVMinOffset + textVDiff * ratio);
+                texts.setScaleX(0.8f + 0.2f * ratio);
+                texts.setScaleY(0.8f + 0.2f * ratio);
+                for (int i = 0; i < textStart.size(); i++) {
+                    texts.getChildAt(i).setX(textStart.get(i) * ratio);
+                }
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getSharedElementEnterTransition().addListener(new Transition.TransitionListener() {
+
+                boolean isStarting = true;
+
+                @Override
+                public void onTransitionStart(Transition transition) {
+                    if (isStarting) {
+                        isStarting = false;
+
+                        ViewCompat.setTransitionName(findViewById(R.id.header_image), null);
+                        ViewCompat.setTransitionName(findViewById(R.id.groupcontrol_nest), null);
+                    }
+                }
+
+                @Override public void onTransitionEnd(Transition transition) {}
+                @Override public void onTransitionCancel(Transition transition) {}
+                @Override public void onTransitionPause(Transition transition) {}
+                @Override public void onTransitionResume(Transition transition) {}
+            });
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        super.onBackPressed();
+        return true;
+    }
+
+    private int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
