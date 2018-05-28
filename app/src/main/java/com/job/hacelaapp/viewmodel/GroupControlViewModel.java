086ca28ad00604c28312f9fc2923aec6ddc8f3a4
JobGetabu <getabujob@gmail.com>
2018-05-28 10:07:40 +0300

    added data viewmodels to group arch

---
+++ app/src/main/java/com/job/hacelaapp/viewmodel/GroupControlViewModel.java
@@ -0,0 +1,9 @@
+package com.job.hacelaapp.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created by Job on Monday : 5/28/2018.
+ */
+public class GroupControlViewModel extends ViewModel {
+}
