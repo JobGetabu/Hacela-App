4d966c99ebf2bdceb7c466cf1bdd09bb32db5b3a
JobGetabu <getabujob@gmail.com>
2018-06-24 06:12:21 +0300

    user transaction pojo

--- app/src/main/java/com/job/hacelaapp/dataSource/UserTransaction.java
+++ app/src/main/java/com/job/hacelaapp/dataSource/UserTransaction.java
@@ -5,4 +5,79 @@ package com.job.hacelaapp.dataSource;
  */
 public class UserTransaction {
 
+    private String userid;
+    private String transactionid;
+    private String type; //  withdraw | deposit
+    private String status; // pending | failed | completed
+    private String timestamp;
+    private double amount;
+
+    public UserTransaction(String userid, String transactionid, String type, String status, String timestamp, double amount) {
+        this.userid = userid;
+        this.transactionid = transactionid;
+        this.type = type;
+        this.status = status;
+        this.timestamp = timestamp;
+        this.amount = amount;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getTransactionid() {
+        return transactionid;
+    }
+
+    public void setTransactionid(String transactionid) {
+        this.transactionid = transactionid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTransaction{" +
+                "userid='" + userid + '\'' +
+                ", transactionid='" + transactionid + '\'' +
+                ", type='" + type + '\'' +
+                ", status='" + status + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
